63ea6b86b37072b4be830cfbc88443de998e21c8
roney ruiz <roneyruizrojas@gmail.com>
2026-07-06 10:16:02 -0600

    chore(app): add tailwind for styling and clean code

---
+++ src/splice.js
@@ -0,0 +1,5 @@
+document.querySelector('#app').innerHTML = `
+  <div class="min-h-screen bg-gray-900 flex items-center justify-center">
+    <h1 class="text-5xl font-bold text-white">Hello World</h1>
+  </div>
+`
